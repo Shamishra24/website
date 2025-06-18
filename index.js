69a181515fd5e64e5d584616974ea34fce4890f9
Shamishra24 <shashwatmishra089@gmail.com>
2025-06-18 23:51:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://first-website1-9nr2.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
